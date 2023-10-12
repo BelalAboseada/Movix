feb8f3933160b84415497cadf35f85a98032101b
Belal Hesham <126694241+BelalAboSeda@users.noreply.github.com>
2023-10-12 04:21:54 +0300

    Update CircleRating.jsx

--- src/components/circleRating/CircleRating.jsx
+++ src/components/circleRating/CircleRating.jsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-
 import "./style.scss";
 
 const CircleRating = ({ rating }) => {
-    return (
-        <div className="circleRating">
-            <CircularProgressbar
-                value={rating}
-                maxValue={10}
-                text={rating}
-                styles={buildStyles({
-                    pathColor:
-                        rating < 5 ? "red" : rating < 7 ? "orange" : "green",
-                })}
-            />
-        </div>
-    );
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) => {
+        const diff = rating - prevProgress;
+        const step = diff > 0 ? 1 : 0;
+        if (prevProgress === rating) {
+          clearInterval(timer);
+        }
+        return prevProgress + step;
+      });
+    }, 100);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [rating]);
+
+  return (
+    <div className="circleRating">
+      <CircularProgressbar
+        value={progress}
+        maxValue={10}
+        text={progress}
+        styles={buildStyles({
+          pathColor:
+            progress < 5 ? "red" : progress < 7 ? "orange" : "green"
+        })}
+      />
+    </div>
+  );
 };
 
 export default CircleRating;
